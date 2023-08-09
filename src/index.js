0f6da6492e19b67c0209e427e15e170415f9b5d4
Baliram <heyome9a@gmail.com>
2023-08-09 14:20:56 +0530

    edited pkg.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React from 'react';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
